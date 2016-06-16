b9d1e8e5bcf539953c6fead29ab3cd92bee59185
Avi Vahl <avi.vahl@wix.com>
2016-06-16 16:05:23 +0300

    Minified bundle

---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var path = require('path');
+var NODE_MODULES_PATH = path.resolve(__dirname, 'node_modules');
+
+var loaders = {
+    loaders: [
+        {
+            test    : /\.ts[x]?$/,
+            exclude : NODE_MODULES_PATH,
+            loader  : 'ts-loader'
+        }
+    ],
+    noParse: /\.min\.js$/
+};
+
+var resolve = {
+    extensions    : ["", ".webpack.js", ".web.js", ".js", ".ts", ".tsx"]
+};
+
+var output = {
+    path          : __dirname + '/dist',
+    filename      : '[name].min.js',
+    libraryTarget : 'umd',
+    library       : '[name]',
+    pathinfo      : true
+};
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        videoholic : ['./src']
+    },
+
+    output: output,
+    resolve: resolve,
+    module: loaders
+};
