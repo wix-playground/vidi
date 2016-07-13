411a79548fcc42582d6a068c7111bb17dd46c9bf
Avi Vahl <avi.vahl@wix.com>
2016-07-14 01:44:01 +0300

    Disable IE tests due to a IE dashjs regression Happened after Microsoft updates. Might only be broken on my local machine (Surface Book). Will install a virtual machine and test.

--- karma.conf.ts
+++ karma.conf.ts
@@ -55,7 +55,7 @@ module.exports = (config) => {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'IE'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
