ba02a6042363b8c0d615f8865471801d9a5a26f8
Avi Vahl <avi.vahl@wix.com>
2016-07-25 12:03:32 +0300

    Add complex case to demo

--- examples/example.tsx
+++ examples/example.tsx
@@ -11,7 +11,14 @@ export function renderDemo(): HTMLElement {
         // DASH: 'http://localhost:3000/sample.mpd',
         DASH: 'http://bitdash-a.akamaihd.net/content/sintel/sintel.mpd',
         MP4: 'http://localhost:3000/sample.mp4',
-        WEBM: 'http://localhost:3000/sample.webm'
+        WEBM: 'http://localhost:3000/sample.webm',
+        COMPLEX: [{ url: 'http://media.wixapps.net/ggl-105826725040924916100/video/1d336a477eae470a8c7d86f59420aa80/480p/mp4/file.mp4', type: 'MP4', name: '480p' },
+            'http://media.wixapps.net/ggl-105826725040924916100/video/1d336a477eae470a8c7d86f59420aa80/720p/mp4/file.mp4',
+            'http://media.wixapps.net/ggl-105826725040924916100/video/1d336a477eae470a8c7d86f59420aa80/1080p/mp4/file.mp4',
+            'http://bitdash-a.akamaihd.net/content/sintel/sintel.mpd',
+            'http://media.wixapps.net/ggl-105826725040924916100/video/1d336a477eae470a8c7d86f59420aa80/480p/webm/file.webm',
+            'http://media.wixapps.net/ggl-105826725040924916100/video/1d336a477eae470a8c7d86f59420aa80/720p/webm/file.webm',
+            'http://media.wixapps.net/ggl-105826725040924916100/video/1d336a477eae470a8c7d86f59420aa80/1080p/webm/file.webm']
     }
 
     const videoStyle = { width: 640, height: 360, background: 'black' }
@@ -23,7 +30,7 @@ export function renderDemo(): HTMLElement {
             super(props);
         }
 
-        changeURL(newUrl: string) {
+        changeURL(newUrl) {
             this.vidi.src = newUrl;
         }
 
@@ -33,6 +40,7 @@ export function renderDemo(): HTMLElement {
                 <button id="DASH" onClick={() => this.changeURL(testStreams.DASH) }>DASH</button>
                 <button id="MP4" onClick={() => this.changeURL(testStreams.MP4) }>MP4</button>
                 <button id="WEBM" onClick={() => this.changeURL(testStreams.WEBM) }>WEBM</button>
+                <button id="COMPLEX" onClick={() => this.changeURL(testStreams.COMPLEX) }>COMPLEX</button>
                 <input type="text" id="customurl" />
                 <button id="custom" onClick={() => this.changeURL((document.getElementById('customurl') as HTMLInputElement).value) }>Custom</button>
                 <br /><br />
