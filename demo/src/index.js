41762d1c9d7401941fdbed393aa33b07be0e5cb9
Avi Vahl <avi.vahl@wix.com>
2016-09-26 01:13:54 +0300

    Update react-scripts Required moving stuff around and cleaning

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-import { App } from './App';
+import {App} from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
