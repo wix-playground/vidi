d4af60798d7c08817c7c1f3e46b0bbf9ddfc3fe4
Avi Vahl <avi.vahl@wix.com>
2016-12-04 17:04:02 +0200

    Update experimental shaka-player integration Apply polyfill, add error handling, and support initial bitrate.

--- src/media-streams/playable-streams/shaka-stream.ts
+++ src/media-streams/playable-streams/shaka-stream.ts
@@ -1,16 +1,20 @@
-import {MediaStream, PlayableStream, MediaStreamTypes, EnvironmentSupport, EmitEventsFn, MediaStreamDeliveryType} from '../../types';
+import { MediaStream, PlayableStream, MediaStreamTypes, EnvironmentSupport, EmitEventsFn, MediaStreamDeliveryType } from '../../types';
 
 const Shaka = require('shaka-player');
-
+Shaka.polyfill.installAll();
 export class ShakaStream implements PlayableStream {
     private shakaPlayer: any = null;
 
     constructor(private mediaStreams: MediaStream[], private emit: EmitEventsFn) {
     }
 
-    public attach(videoElement: HTMLVideoElement) {
+    public attach(videoElement: HTMLVideoElement, initialBitrate: number) {
         this.shakaPlayer = new Shaka.Player(videoElement);
         this.shakaPlayer.load(this.mediaStreams[0].url)
+        this.shakaPlayer.addEventListener('error', (e) => this.emit('error', e));
+        if (initialBitrate) {
+            this.shakaPlayer.configure({ abr: { defaultBandwidthEstimate: initialBitrate } });
+        }
     }
 
     public detach(videoElement: HTMLVideoElement) {
@@ -23,7 +27,7 @@ export class ShakaStream implements PlayableStream {
     }
 
     public static isSupported(env: EnvironmentSupport): boolean {
-        return env.MSE;
+        return Shaka.Player.isBrowserSupported();
     };
 
     public static canPlay(mediaType: string): boolean {
