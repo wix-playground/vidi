52c97352d8df3ef11698cc4b894bd31c237c63ff
Avi Vahl <avi.vahl@wix.com>
2016-12-04 16:58:37 +0200

    Improve readability

--- demo/src/App.js
+++ demo/src/App.js
@@ -7,37 +7,34 @@ export class App extends Component {
     super(props);
     this.state = { format: 'Auto', sourceUrl: '', dropdownOpen: false };
     this.vidi = new Vidi();
-    this.play = this.play.bind(this);
-    this.handleVideoRef = this.handleVideoRef.bind(this);
   }
 
   setFormat(format) {
     this.setState({ format, dropdownOpen: false });
   }
 
-  play() {
+  play = () => {
     switch (this.state.format) {
       case 'Hls':
-        this.vidi.src = {type: Vidi.MediaStreamTypes.HLS, url: this.state.sourceUrl};
+        this.vidi.src = { type: Vidi.MediaStreamTypes.HLS, url: this.state.sourceUrl };
         break;
       case 'Dash':
-        this.vidi.src = {type: Vidi.MediaStreamTypes.DASH, url: this.state.sourceUrl};
+        this.vidi.src = { type: Vidi.MediaStreamTypes.DASH, url: this.state.sourceUrl };
         break;
       case 'Mp4':
-        this.vidi.src = {type: Vidi.MediaStreamTypes.MP4, url: this.state.sourceUrl};
+        this.vidi.src = { type: Vidi.MediaStreamTypes.MP4, url: this.state.sourceUrl };
         break;
       case 'WebM':
-        this.vidi.src = {type: Vidi.MediaStreamTypes.WEBM, url: this.state.sourceUrl};
+        this.vidi.src = { type: Vidi.MediaStreamTypes.WEBM, url: this.state.sourceUrl };
         break;
       default:
         this.vidi.src = this.state.sourceUrl;
         break;
     }
-    this.vidi.pause();
     this.vidi.play();
   }
 
-  handleVideoRef(videoElement) {
+  handleVideoRef = (videoElement) => {
     this.vidi.setVideoElement(videoElement);
   }
 
@@ -45,11 +42,11 @@ export class App extends Component {
 
     const dropdownContent = !this.state.dropdownOpen ? null :
       <div className="dropdown-content">
-        <div onClick={() => this.setFormat('Auto') }>Auto</div>
-        <div onClick={() => this.setFormat('Hls') }>Hls</div>
-        <div onClick={() => this.setFormat('Dash') }>Dash</div>
-        <div onClick={() => this.setFormat('Mp4') }>Mp4</div>
-        <div onClick={() => this.setFormat('WebM') }>WebM</div>
+        <div onClick={() => this.setFormat('Auto')}>Auto</div>
+        <div onClick={() => this.setFormat('Hls')}>Hls</div>
+        <div onClick={() => this.setFormat('Dash')}>Dash</div>
+        <div onClick={() => this.setFormat('Mp4')}>Mp4</div>
+        <div onClick={() => this.setFormat('WebM')}>WebM</div>
       </div>;
 
     return (
@@ -60,18 +57,18 @@ export class App extends Component {
           <video className="video" key="video" controls ref={this.handleVideoRef} />
           <div className="formWrapper">
             <label htmlFor="sourceInput" className="urlLabel">URL: </label>
-            <input type="text" id="sourceInput" onChange={e => this.setState({ sourceUrl: e.target.value }) } value={this.state.sourceUrl} />
-            <div className="dropdown" onClick={() => this.setState({ dropdownOpen: !this.state.dropdownOpen }) } onMouseEnter={() => this.setState({ dropdownOpen: true }) } onMouseLeave={() => this.setState({ dropdownOpen: false }) }>
+            <input type="text" id="sourceInput" onChange={e => this.setState({ sourceUrl: e.target.value })} value={this.state.sourceUrl} />
+            <div className="dropdown" onClick={() => this.setState({ dropdownOpen: !this.state.dropdownOpen })} onMouseEnter={() => this.setState({ dropdownOpen: true })} onMouseLeave={() => this.setState({ dropdownOpen: false })}>
               <div className="btn dropbtn">{this.state.format}</div>
               {dropdownContent}
             </div>
             <span className="btn playBtn" onClick={this.play}>Play</span>
           </div>
           <div className="boxes">
-            <div className="box" onClick={() => this.setState({ sourceUrl: 'http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8' }) }>
+            <div className="box" onClick={() => this.setState({ sourceUrl: 'http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8' })}>
               <img src="https://raw.githubusercontent.com/wix/vidi/master/demo/assets/BigBuckBunny.jpg" title="Big Buck Bunny via HLS" alt="Big Buck Bunny" />
             </div>
-            <div className="box centerBox" onClick={() => this.setState({ sourceUrl: 'http://bitdash-a.akamaihd.net/content/sintel/sintel.mpd' }) }>
+            <div className="box centerBox" onClick={() => this.setState({ sourceUrl: 'http://bitdash-a.akamaihd.net/content/sintel/sintel.mpd' })}>
               <img src="https://raw.githubusercontent.com/wix/vidi/master/demo/assets/Sintel.jpg" title="Sintel via MPEG-DASH" alt="Sintel" />
             </div>
             <div className="box">
