0942644981f296d1a653d75a98d23cdd1d60b522
Avi Vahl <avi.vahl@wix.com>
2017-02-23 01:35:40 +0200

    Upgrade all dependencies Fixed ts2.2.1 compat in test media server

--- http-media-server/http-media-server.ts
+++ http-media-server/http-media-server.ts
@@ -24,8 +24,8 @@ interface ReadStreamOptions {
 }
 
 interface RangeRequest {
-    start?: number;
-    end?: number;
+    start: number;
+    end: number;
 }
 
 function readRangeHeader(range: string, totalLength: number): RangeRequest | null {
@@ -88,7 +88,7 @@ export function startHttpMediaServer(port = 3000, onListen: Function = () => { }
                 const ext = path.extname(uri).slice(1);
                 const mimeType = mimeTypes[ext];
                 const fileSize = fs.statSync(fileName).size;
-                const rangeRequest: RangeRequest | null = readRangeHeader(req.headers['range'], fileSize);
+                const rangeRequest: RangeRequest | null = readRangeHeader(req.headers.range, fileSize);
 
                 if (mimeType) {
                     const options: ReadStreamOptions = {};
