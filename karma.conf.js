c2b11821fdec2aa2242cc425ae39d43397a360fd
Avi Vahl <avi.vahl@wix.com>
2018-01-21 18:45:19 +0200

    Use --no-sandbox to launch Chrome on travis per https://docs.travis-ci.com/user/chrome

--- karma.conf.js
+++ karma.conf.js
@@ -64,8 +64,14 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['ChromeHeadless', 'FirefoxHeadless'],
+        browsers: ['ChromeHeadlessNoSandbox', 'FirefoxHeadless'],
 
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox']
+            }
+        },
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
